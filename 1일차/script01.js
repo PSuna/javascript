7f8b19cf02095f360aed7f49e6870b908f5bb21a
Suna <xkq3355@naver.com>
2022-12-20 10:37:48 +0900

    first commit

---
+++ 1일차/script01.js
@@ -0,0 +1,6 @@
+let heading = document.querySelector("#heading");
+heading.onclick = function(){
+  // heading.style.backgroundColor = 'yellow'
+  //heading.style.color = 'red'
+  heading.style.display = 'none';
+}
