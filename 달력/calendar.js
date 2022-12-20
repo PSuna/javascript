20e1cdc679d0ffe99a4ac71b58903311be9e4eee
Suna <xkq3355@naver.com>
2022-12-20 23:53:55 +0900

    코드분석1

--- 달력/calendar.js
+++ 달력/calendar.js
@@ -1,11 +1,15 @@
 // 달력 생성
-const makeCalendar = (date) => {
+const makeCalendar = (date) => { //람다식, date : 오늘 날짜
   // 현재 년도와 월 받아오기
-  const currentYear = new Date(date).getFullYear();
-  const currentMonth = new Date(date).getMonth() + 1;
+  const currentYear = new Date(date).getFullYear(); // 현재년도
+  
+  //현재월 : getMonth()는 월의 index값이 반환되므로 +1을 더해주어야함
+  const currentMonth = new Date(date).getMonth() + 1; 
 
   // 첫날의 요일 구하기 - 초기 시작위치를 위해서
+  // getDay() 요일 구하는 메서드
   const firstDay = new Date(date.setDate(1)).getDay();
+
   // 마지막 날짜 구하기
   const lastDay = new Date(currentYear, currentMonth, 0).getDate();
 
@@ -35,7 +39,7 @@ const makeCalendar = (date) => {
 }
 
 
-const date = new Date();
+const date = new Date(); // 오늘 날짜
 
 makeCalendar(date);
 
